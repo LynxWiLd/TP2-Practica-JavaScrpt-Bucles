0fb21ac331b0a5ec98ff1072611d89d2e20be345
Vera_Facundo <facundovera647@gmail.com>
2025-09-06 20:08:50 -0300

    Inicializo el repositorio

---
+++ js/ejercicio-01.js
@@ -0,0 +1,13 @@
+//Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+const edad = parseInt(prompt("Ingrese su edad:"));
+
+if (!isNaN(edad)) {     
+    if (edad >= 18) {
+        document.writeln("Usted ya puede conducir.");
+    } else {
+        document.writeln("Usted no puede conducir.");
+    }   
+} else {
+    document.writeln("Por favor, ingrese un número válido.");
+}
---
+++ js/ejercicio-02.js
@@ -0,0 +1,34 @@
+/*
+Escribir un programa que solicite una nota (número) de 0  a 10. Luego mostrar la calificación en un alert según los siguientes rangos de nota:
+
+0-2: Muy deficiente
+3-4: Insuficiente
+5-6: Suficiente
+7: Bien
+8-9: Notable
+10: Sobresaliente
+
+Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.
+
+Ejemplo:
+
+Input: 5 Output: Suficiente
+Input: 50 Output: Número erróneo
+Input: hola10 Output: Introduce un número válido 
+
+*/
+
+const nota = parseInt(prompt("Ingrese una nota del 0 al 10:"));
+if (!isNaN(nota)) {
+  if (nota >= 0 && nota <= 10) {
+    if (nota >= 3 && nota <= 4) alert("Insuficiente");
+    if (nota >= 5 && nota <= 6) alert("Suficiente");
+    if (nota === 7) alert("Bien");
+    if (nota >= 8 && nota <= 9) alert("Notable");
+    if (nota === 10) alert("Sobresaliente");
+  } else {
+    document.writeln("Número erróneo");
+  }
+}else {
+  document.writeln("Introduce un número válido");
+}
