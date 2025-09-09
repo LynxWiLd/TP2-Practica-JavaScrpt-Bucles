0a45052eccc68d625d2edd3dba84f4940b9bf602
Vera_Facundo <facundovera647@gmail.com>
2025-09-08 22:58:18 -0300

    ejercicio5 terminado y listo para merge

--- js/ejercicio-05.js
+++ js/ejercicio-05.js
@@ -15,58 +15,63 @@ Input:  40773821 ------------ Output: ‘L’
 
 let numero = 0;
 let letra = "";
+
 do {
-  if (numer >= 0 && numero <= 99999999) {
-    let resto = numero % 23;
-    if (resto === 0) {
-      letra = "T";
-    } else if (resto === 1) {
-      letra = "R";
-    } else if (resto === 2) {
-      letra = "W";
-    } else if (resto === 3) {
-      letra = "A";
-    } else if (resto === 4) {
-      letra = "G";
-    } else if (resto === 5) {
-      letra = "M";
-    } else if (resto === 6) {
-      letra = "Y";
-    } else if (resto === 7) {
-      letra = "F";
-    } else if (resto === 8) {
-      letra = "P";
-    } else if (resto === 9) {
-      letra = "D";
-    } else if (resto === 10) {
-      letra = "X";
-    } else if (resto === 11) {
-      letra = "B";
-    } else if (resto === 12) {
-      letra = "N";
-    } else if (resto === 13) {
-      letra = "J";
-    } else if (resto === 14) {
-      letra = "Z";
-    } else if (resto === 15) {
-      letra = "S";
-    } else if (resto === 16) {
-      letra = "Q";
-    } else if (resto === 17) {
-      letra = "V";
-    } else if (resto === 18) {
-      letra = "H";
-    } else if (resto === 19) {
-      letra = "L";
-    } else if (resto === 20) {
-      letra = "C";
-    } else if (resto === 21) {
-      letra = "K";
-    } else if (resto === 22) {
-      letra = "E";
+  numero = prompt("Introduce un número de DNI");
+
+  if (isNaN(numero)) {
+    alert("No es un número, inténtelo de nuevo.");
+  } else if (numero >= 0 && numero <= 99999999) {
+      let resto = numero % 23;
+      if (resto === 0) {
+        letra = "T";
+      } else if (resto === 1) {
+        letra = "R";
+      } else if (resto === 2) {
+        letra = "W";
+      } else if (resto === 3) {
+        letra = "A";
+      } else if (resto === 4) {
+        letra = "G";
+      } else if (resto === 5) {
+        letra = "M";
+      } else if (resto === 6) {
+        letra = "Y";
+      } else if (resto === 7) {
+        letra = "F";
+      } else if (resto === 8) {
+        letra = "P";
+      } else if (resto === 9) {
+        letra = "D";
+      } else if (resto === 10) {
+        letra = "X";
+      } else if (resto === 11) {
+        letra = "B";
+      } else if (resto === 12) {
+        letra = "N";
+      } else if (resto === 13) {
+        letra = "J";
+      } else if (resto === 14) {
+        letra = "Z";
+      } else if (resto === 15) {
+        letra = "S";
+      } else if (resto === 16) {
+        letra = "Q";
+      } else if (resto === 17) {
+        letra = "V";
+      } else if (resto === 18) {
+        letra = "H";
+      } else if (resto === 19) {
+        letra = "L";
+      } else if (resto === 20) {
+        letra = "C";
+      } else if (resto === 21) {
+        letra = "K";
+      } else if (resto === 22) {
+        letra = "E";
+      }
+      alert("La letra correspondiente al número " + numero + " es: " + letra);
+    } else {
+      alert("El número introducido no es válido, inténtelo de nuevo con un numéro entre 0 y 99999999.");
     }
-    alert("La letra correspondiente al número " + numero + " es: " + letra);
-  } else {
-    alert("El número introducido no es válido");
-  }
-} while (confirm("¿Desea continuar?"));
+} while (confirm("¿Desea continuar ingresando DNI?"));
