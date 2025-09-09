dc5345b702a0a966e19fd1c1dacdcb8ee70e72a4
Vera_Facundo <facundovera647@gmail.com>
2025-09-09 02:32:43 -0300

    termine ejercicio17

--- js/ejercicio-17.js
+++ js/ejercicio-17.js
@@ -7,3 +7,26 @@ Ejemplo:
 Input: Hola mundo ------------- Output: la vocal ‘o’ está en la posición 1
 
 */
+
+let texto = prompt("Ingrese un texto:");
+let posicion = -1;
+for (let i = 0; i < texto.length; i++) {
+  let char = texto.charAt(i).toLowerCase();
+  if (
+    char === "a" ||
+    char === "e" ||
+    char === "i" ||
+    char === "o" ||
+    char === "u"
+  ) {
+    posicion = i+1;
+    break;
+  }
+}
+if (posicion !== -1) {
+  document.writeln(
+    "La primera vocal de " + texto + " está en la posición: " + posicion
+  );
+} else {
+  document.writeln("No se encontraron vocales en el texto.");
+}
