98f5be09fb5698c0acc4684f02a7f3e2d6bd0406
Vera_Facundo <facundovera647@gmail.com>
2025-09-09 11:33:15 -0300

    ejerciccio16 terminado y listo para el merge

--- js/ejercicio-16.js
+++ js/ejercicio-16.js
@@ -1,2 +1,9 @@
 /*Realiza un script que pida una cadena de texto y la devuelva al revés. Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
  */
+let texto = prompt("Ingrese un texto:");
+let textoInvertido = "";
+for (let i = texto.length - 1; i >= 0; i--) {
+    textoInvertido += texto.charAt(i);
+}
+document.writeln("El texto invertido es: " + textoInvertido);
+    
