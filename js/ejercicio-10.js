5b7e4bf82f7d98d2e9ab418ee196df6573f86a05
Vera_Facundo <facundovera647@gmail.com>
2025-09-09 11:14:34 -0300

    ejercicio10 terminado, listo merge dev

--- js/ejercicio-10.js
+++ js/ejercicio-10.js
@@ -1,2 +1,17 @@
 /*Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
  */
+
+const filas = parseInt(prompt("Ingrese el número de filas:"));
+const columnas = parseInt(prompt("Ingrese el número de columnas:"));
+const totalCeldas = filas * columnas;
+let numero = totalCeldas;
+document.writeln(`<table class="table striped-columns"><tbody>`);
+for (let i = 0; i < filas; i++) {
+  document.writeln(`<tr>`);
+  for (let j = 0; j < columnas; j++) {
+    document.writeln(`<td>` + numero + `</td>`);
+    numero--;
+  }
+  document.writeln(`</tr>`);
+}
+document.writeln(`</table></tbody>`);
